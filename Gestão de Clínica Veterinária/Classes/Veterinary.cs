56cbcd05262e4a5bc7679a0b78f2c0fc71ba8407
Zepaposeco <marcos.dss92@gmail.com>
2021-03-29 12:28:01 +0100

    Update classes

---
+++ Gestão de Clínica Veterinária/Classes/Veterinary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gestão_de_Clínica_Veterinária.Classes
+{
+    class Veterinary
+    {
+        private int Id { get; }
+        private string Name { get; set; }
+        public string Dia { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFim { get; set; }
+
+        public Veterinary (int id, string name, string dia, string horaInicio, string horaFim)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Dia = dia;
+            this.HoraInicio = horaInicio;
+            this.HoraFim = horaFim;
+        }
+
+        public bool Equals (Veterinary veterinary)
+        {
+            bool result;
+
+            if (veterinary == null){ result = false;}
+            else{ result = Id.Equals(veterinary.Id) ? true : false; }
+
+            return result;
+        }
+
+        public override String ToString()
+        {
+            string text;
+
+            text = "Id: " + this.Id + "\n" + "Nome: " + this.Name;
+
+            return text;
+        }
+    }
+}
