f193df5461ea7ec6d5303059174356100ca047d2
marcosdss92 <marcos.dss92@gmail.com>
2021-03-30 12:00:17 +0100

    ed

---
+++ Gestão de Clínica Veterinária/Classes/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Gestão_de_Clínica_Veterinária.Classes
+{
+    class FileReader
+    {
+       
+        public Veterinary[] ReadVeterinary()
+        {
+            string[] dirs = Directory.GetFiles(@"C:\Temp\", "*.csv");
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+        }
+       
+    }
+}
