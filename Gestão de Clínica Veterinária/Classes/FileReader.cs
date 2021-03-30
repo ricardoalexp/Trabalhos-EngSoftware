813d0e617ab8da252ca4986c06979562d32a4116
marcosdss92 <marcos.dss92@gmail.com>
2021-03-30 12:37:36 +0100

    sasa

--- Gestão de Clínica Veterinária/Classes/FileReader.cs
+++ Gestão de Clínica Veterinária/Classes/FileReader.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Gestão_de_Clínica_Veterinária.Classes
 {
     class FileReader
     {
        
-        public Veterinary[] ReadVeterinary()
+        public List<Veterinary> ReadVeterinary()
         {
-            string[] dirs = Directory.GetFiles(@"C:\Temp\", "*.csv");
+            string[] dirs = Directory.GetFiles(@"Resources\Clients\*.txt");
+            List<Veterinary> vetList = new List<Veterinary>();
+
             foreach (string dir in dirs)
             {
-                Console.WriteLine(dir);
+                string text = File.ReadAllText(dir);
+                string[] atributes = text.Split(';');
+                
+                Veterinary vet = new Veterinary(int.Parse(atributes[0]), atributes[1]);
+
+                vetList.Add(vet);
             }
+            return vetList;
         }
        
     }
